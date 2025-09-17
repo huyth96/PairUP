75aea8fc378a26eca95742c61e55083a486825d6
Huy <Huyth.work@gmail.com>
2025-09-17 17:44:01 +0700

    Fix double click

---
+++ Assets/Scripts/Presentation/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelToggle : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject targetPanel;   // Panel cần bật/tắt
+    [SerializeField] private Button openButton;        // Nút mở
+    [SerializeField] private Button closeButton;       // Nút đóng
+
+    void Awake()
+    {
+        if (openButton) openButton.onClick.AddListener(OpenPanel);
+        if (closeButton) closeButton.onClick.AddListener(ClosePanel);
+
+        // Đảm bảo panel ban đầu ẩn
+        if (targetPanel) targetPanel.SetActive(false);
+    }
+
+    public void OpenPanel()
+    {
+        if (targetPanel) targetPanel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        if (targetPanel) targetPanel.SetActive(false);
+    }
+}
